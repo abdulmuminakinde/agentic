17e1b7e02f0004b19d20790a2d14e86b34ec9108
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-06-14 11:25:38 +0100

    feat: add command line prompt and exception handling
    The program now receives prompts from the command line.
    If no prompt is provided, the program will ask the user for a prompt in the terminal.
    Adds KeyboardInterrupt exception handling to allow the user to exit the program.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -8,26 +9,29 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 
-async def generate_text(prompt):
-    client = genai.Client(api_key=api_key)
+async def generate_text():
+    try:
+        prompt = " ".join(sys.argv[1:])
+        if prompt == "":
+            prompt = input("What is your prompt?\n>> ")
+        suffix = "Reply in just one paragraph."
+        client = genai.Client(api_key=api_key)
 
-    response = await client.aio.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=prompt,
-    )
+        response = await client.aio.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=f"{prompt} {suffix}",
+        )
 
-    print(response.text)
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n Response tokenss: {response.usage_metadata.candidates_token_count}"
-    )
+        print(response.text)
+        print(
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n Response tokenss: {response.usage_metadata.candidates_token_count}"
+        )
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 
 async def main():
-    task = asyncio.create_task(
-        generate_text(
-            "Is cloud computing the same as cloud engineering? Reply in one paragraph maximum."
-        )
-    )
+    task = asyncio.create_task(generate_text())
     print("Hello from agentic!")
     await task
 
