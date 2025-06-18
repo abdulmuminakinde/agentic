7c05f9bf573866be0ac1a8c0d0234cb7d63761e7
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-06-18 19:40:21 +0100

    feat: implement function calling for file system operations
    This commit introduces function calling to the agent, allowing it to interact with the file system.
    
    The following changes were made:
    - Added `call_function.py` to define available functions, using `schema_get_files_info` for file
    listing.
    - Updated `config.py` with a `system_prompt` to guide the agent's behavior and instruct it to use
    file operations.
    - Modified `functions/get_files_info.py` to include a schema definition for `get_files_info`
    function.
    - Refactored `main.py` to use the `available_functions` and `system_prompt` to make calls with the
    model.
    - `main.py` now handles prompt and verbosity as command-line arguments.
    - Removed asyncio from `main.py` for simipler flow.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import sys
 
@@ -6,43 +5,60 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+from call_function import available_functions
+from config import system_prompt
 
 
-async def generate_text():
-    try:
-        prompt = " ".join(sys.argv[1:])
-        if prompt == "":
-            prompt = input("What is your prompt?\n>> ")
-        if prompt == "":
-            print("No prompt provided. Please provide a prompt.")
-            sys.exit(1)
-        suffix = " Reply in just one paragraph."
-        client = genai.Client(api_key=api_key)
+def generate_text(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
+    )
 
-        messages = [
-            types.Content(role="user", parts=[types.Part(text=prompt + suffix)])
-        ]
-
-        response = await client.aio.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-        )
-
-        print(response.text)
+    if verbose:
         print(
             f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n Response tokenss: {response.usage_metadata.candidates_token_count}"
         )
+
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
+
+
+def main():
+
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    try:
+        verbose = "--verbose" in sys.argv
+        args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+        if not args:
+            print("AI Code Assistant")
+            print("\nUsage: python main.py <prompt>")
+            print('Example: python main.py "3 + 5"')
+
+        prompt = " ".join(args)
+        if prompt == "":
+            prompt = input("What is your prompt?\nPress Ctrl+C to exit.\n>> ")
+        if prompt == "":
+            print("No prompt provided. Please provide a prompt.")
+            sys.exit(1)
+        client = genai.Client(api_key=api_key)
+
+        messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+        generate_text(client, messages, verbose)
     except KeyboardInterrupt:
-        sys.exit(0)
-
-
-async def main():
-    task = asyncio.create_task(generate_text())
-    print("Hello from agentic!")
-    await task
+        print("\nGoodbye!")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
