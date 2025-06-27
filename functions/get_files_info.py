d6906e11f046b476f58eec45ad85c35f78f53492
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-06-27 10:50:12 +0100

    fix: Ensure `is_dir` is called and improve error message

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -22,8 +22,11 @@ def get_files_info(working_directory, directory=None):
     except ValueError:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not target_dir.is_dir:
-        return f'Error: "{directory}" is not a directory'
+    if not target_dir.is_dir():
+        if directory:
+            return f'Error: "{directory}" is not a directory'
+        else:
+            return f'Error: "{working_directory}" is not a directory'
 
     try:
         files_info = []
